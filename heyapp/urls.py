8a9741526f049ac4e863c9b60e503049f97d20da
Apondi-Otieno <apondiotii@gmail.com>
2022-03-28 01:38:36 +0300

    this will add url patterns

--- heyapp/urls.py
+++ heyapp/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+# urlpatterns = [
+    
+#     path('admin/', admin.site.urls),
+# ]
+
+urlpatterns=[
+    url(r'^admin/', admin.site.urls),
+    url(r'^gallery/',include('gallery.urls'))
 ]
+
