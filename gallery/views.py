596541422ac74241d77bbbbafa4a61b18b67d3c2
Apondi-Otieno <apondiotii@gmail.com>
2022-03-28 09:17:22 +0300

    this will define function views to enable the user view pictures on the app

--- gallery/views.py
+++ gallery/views.py
@@ -3,5 +3,24 @@ from .models import Category, Picture
 from django.http import HttpResponse,Http404
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my gallery!')
+
+
+
+def home(request):
+    picture=Picture.objects.all()   
+    return render(request, 'index.html',{'picture':picture[::-1],})
+
+
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        searched_articles = Picture.search_by_category( category)
+        message = f"{ category}"
+
+        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
