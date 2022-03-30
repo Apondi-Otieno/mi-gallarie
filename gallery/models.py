2a99edb0de3ad44a367bf3205f89d9005d3d2aca
Apondi-Otieno <apondiotii@gmail.com>
2022-03-30 15:26:53 +0300

    this will add cloudinary field import

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from cloudinary.models import cloudinaryField
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200 , unique=True)
